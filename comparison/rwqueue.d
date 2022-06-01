4c83932e16ea6e67f19b845bbd58a3a6fee31ce1
yilabs <yilabs.com@gmail.com>
2022-06-01 18:43:36 -0400

    fix std.datetime.stopwatch

--- comparison/rwqueue.d
+++ comparison/rwqueue.d
@@ -2,6 +2,7 @@
 /+ dub.sdl:
 name "app"
 dependency "lock-free" version="~>0.1.2"
+dependency "dests" version="0.3.1"
 dflags "-release" "-m64" "-boundscheck=off" "-O" platform="dmd"
 dflags "-O4" "--release" "--boundscheck=off" platform="ldc2"
 +/
@@ -12,12 +13,13 @@ dflags "-O4" "--release" "--boundscheck=off" platform="ldc2"
 
 import std.stdio;
 import std.datetime;
+import std.datetime.stopwatch;
 import core.atomic;
 import core.thread, std.concurrency;
 import std.process;
 
 
-const n=1_000_000_000; //;_000
+const n=1_000_000_000; //; //;_000
 enum amount = n;
 
 void push(Q, T)(ref shared(Q) queue) {
@@ -26,7 +28,7 @@ void push(Q, T)(ref shared(Q) queue) {
         {
             while (queue.full)
                 Thread.yield();
-            queue.push(cast(shared T)i);
+            queue.push/*!T*/(cast(shared T)i);
         }
 }
 
@@ -40,12 +42,12 @@ void pop(Q, T)(ref shared(Q) queue) {
             while (queue.empty)
                 Thread.yield();
 	    auto p = queue.pop();
-            //assert(p.i == cast(shared T)i);
-	    s += p.i;
+            assert(p == i);  // p.i
+	    s += p;
         }
    sw.stop();
    writeln("finished receiving");
-   writefln("received %d messages in %d msec sum=%d speed=%d msg/msec", n, sw.peek().msecs, s, n/sw.peek().msecs);
+   writefln("received %d messages in %d msec sum=%d speed=%d msg/msec", n, sw.peek.total!"msecs", s, n/sw.peek.total!"msecs");
 }
 
 
@@ -56,17 +58,30 @@ void run_test(Q, T)(ref shared(Q) queue) {
     t1.start();
     t0.join();
     t1.join();
+    thread_joinAll();
 }
+
+//struct Data { size_t i; }
+
+alias Data = size_t;
+
 
 void test_lock_free_rwqueue() {
     import lock_free.rwqueue;
-    static struct Data { size_t i; }
     shared(RWQueue!Data) queue;
     run_test!(RWQueue!Data, Data)(queue);
 }
 
+/*
+void test_jin_go_queue() {
+    import jin_go_queue;
+    shared Queue!Data queue = new Queue!Data;
+    run_test!(Queue!Data, Data)(queue);
+}
+*/
 
 void main() {
   test_lock_free_rwqueue();
+  // test_jin_go_queue();
 }
 
