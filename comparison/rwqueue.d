f2b344e646a1e336732b3ce38a4b40b9988b79db
yilabs <yilabs.com@gmail.com>
2022-06-03 23:53:32 -0400

    add test_rust_queue()

--- comparison/rwqueue.d
+++ comparison/rwqueue.d
@@ -3,14 +3,18 @@
 name "app"
 dependency "lock-free" version="~>0.1.2"
 dependency "dests" version="0.3.1"
+dependency "rust_interop_d" version="0.2.2"
 dflags "-release" "-m64" "-boundscheck=off" "-O" platform="dmd"
 dflags "-O4" "--release" "--boundscheck=off" platform="ldc2"
 +/
 
+/*
+ */
 // dmd.exe  -release -m64 -boundscheck=off -O  rwqueue.d
 // ldc2 -O4 --release --boundscheck=off rwqueue.d
 
 
+import std.exception;
 import std.stdio;
 import std.datetime;
 import std.datetime.stopwatch;
@@ -19,7 +23,7 @@ import core.thread, std.concurrency;
 import std.process;
 
 
-const n=1_000_000_000; //; //;_000
+const long n=100_000_000; //; //; //;_000
 enum amount = n;
 
 void push(Q, T)(ref shared(Q) queue) {
@@ -28,7 +32,7 @@ void push(Q, T)(ref shared(Q) queue) {
         {
             while (queue.full)
                 Thread.yield();
-            queue.push/*!T*/(cast(shared T)i);
+            queue.push(/*cast(shared T)*/i);
         }
 }
 
@@ -36,18 +40,20 @@ void pop(Q, T)(ref shared(Q) queue) {
    writeln("c tid:", thisThreadID);
    StopWatch sw;
    sw.start();  
-   long s = 0;
+   long sum = 0;
         foreach (i; 0 .. amount)
         {
             while (queue.empty)
                 Thread.yield();
 	    auto p = queue.pop();
-            assert(p == i);  // p.i
-	    s += p;
+            enforce(p == i);  // p.i
+	    sum += p;
         }
    sw.stop();
+   long expected = (n / 2) * (n-1);  // avoid overflow
    writeln("finished receiving");
-   writefln("received %d messages in %d msec sum=%d speed=%d msg/msec", n, sw.peek.total!"msecs", s, n/sw.peek.total!"msecs");
+   writefln("received %d messages in %d msec sum:%d speed=%d msg/msec; %d", n, sw.peek.total!"msecs", sum, n/sw.peek.total!"msecs", expected);
+   enforce(expected == sum);
 }
 
 
@@ -66,6 +72,7 @@ void run_test(Q, T)(ref shared(Q) queue) {
 alias Data = size_t;
 
 
+// received 100000000 messages in 1339 msec sum:4999999950000000 speed=74682 msg/msec; 4999999950000000
 void test_lock_free_rwqueue() {
     import lock_free.rwqueue;
     shared(RWQueue!Data) queue;
@@ -80,8 +87,16 @@ void test_jin_go_queue() {
 }
 */
 
+// received 100000000 messages in 18938 msec sum:4999999950000000 speed=5280 msg/msec; 4999999950000000
+void test_rust_queue() {
+    import rust_interop;
+    auto queue = new shared(SegQueue);
+    run_test!(SegQueue, Data)(queue);
+}
+
 void main() {
   test_lock_free_rwqueue();
   // test_jin_go_queue();
+  test_rust_queue();
 }
 
